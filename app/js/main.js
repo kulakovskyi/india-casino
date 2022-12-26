fe532da0c957403e6bcba175c4aae12982059ce1
kulakovskyi <torchok9@gmail.com>
2022-12-26 13:46:28 +0200

    poleteli nahui

---
+++ app/js/main.js
@@ -0,0 +1,165 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/_components.js":
+/*!*******************************!*\
+  !*** ./src/js/_components.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_ex__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/ex */ "./src/js/components/ex.js");
+/* harmony import */ var _components_ex__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_components_ex__WEBPACK_IMPORTED_MODULE_0__);
+
+
+/***/ }),
+
+/***/ "./src/js/_functions.js":
+/*!******************************!*\
+  !*** ./src/js/_functions.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _functions_burger__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./functions/burger */ "./src/js/functions/burger.js");
+/* harmony import */ var _functions_burger__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_functions_burger__WEBPACK_IMPORTED_MODULE_0__);
+//Реализация бургер-меню
+
+
+// Реализация анимации появления при скрролле
+//import { animToScroll } from './functions/animToScroll';
+
+// Реализация скрола по якорю
+//import { scrollAnchors } from './functions/scrollAnchors';
+
+// Реализация аккордеона
+//import { accorderon } from './functions/accorderon';
+
+// Реализация табов
+//import { tabs } from './functions/tabs';
+
+/***/ }),
+
+/***/ "./src/js/components/ex.js":
+/*!*********************************!*\
+  !*** ./src/js/components/ex.js ***!
+  \*********************************/
+/***/ (() => {
+
+console.log('Component');
+
+/***/ }),
+
+/***/ "./src/js/functions/burger.js":
+/*!************************************!*\
+  !*** ./src/js/functions/burger.js ***!
+  \************************************/
+/***/ (() => {
+
+const iconMenu = document.querySelector(".menu__icon");
+const iconMenuSpan = document.querySelector(".menu__icon > span");
+const menuBody = document.querySelector(".header__menu");
+const body = document.querySelector("body");
+iconMenu.addEventListener("click", function (e) {
+  iconMenu.classList.toggle("_active");
+  menuBody.classList.toggle("_active");
+  body.classList.toggle("_lock");
+  // заекрытие меню по клику на любую область
+  // дополниельно делаем проверку клик по спану, что бы отрабатывало закрытие
+  window.onclick = function (e) {
+    if (e.target !== menuBody && e.target !== iconMenu && e.target !== iconMenuSpan) {
+      menuBody.classList.remove("_active");
+      iconMenu.classList.remove("_active");
+    }
+  };
+});
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _functions__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./_functions */ "./src/js/_functions.js");
+/* harmony import */ var _components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./_components */ "./src/js/_components.js");
+
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ src/js/_functions.js
@@ -0,0 +1,17 @@
+//Реализация бургер-меню
+import { burger } from './functions/burger';
+
+// Реализация анимации появления при скрролле
+//import { animToScroll } from './functions/animToScroll';
+
+// Реализация скрола по якорю
+//import { scrollAnchors } from './functions/scrollAnchors';
+
+// Реализация аккордеона
+//import { accorderon } from './functions/accorderon';
+
+// Реализация табов
+//import { tabs } from './functions/tabs';
+
+
+
