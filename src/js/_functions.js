61325e1587bc04cc7a04d3063235a3a9e892ab51
kulakovskyi <torchok9@gmail.com>
2022-12-28 17:01:29 +0200

    work in home page

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,17 +1,12 @@
 //Реализация бургер-меню
 import { burger } from './functions/burger';
 
-// Реализация анимации появления при скрролле
-//import { animToScroll } from './functions/animToScroll';
-
 // Реализация скрола по якорю
 //import { scrollAnchors } from './functions/scrollAnchors';
 
 // Реализация аккордеона
-//import { accorderon } from './functions/accorderon';
+import { accorderon } from './functions/accorderon';
 
-// Реализация табов
-//import { tabs } from './functions/tabs';
 
 
 
