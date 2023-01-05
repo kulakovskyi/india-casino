39eb1cb979f31edb0775ec1ce6236c220b7445ae
kulakovskyi <torchok9@gmail.com>
2023-01-05 16:30:47 +0200

    add page16

---
+++ src/js/script-page16.js
@@ -0,0 +1,17 @@
+//Реализация бургер-меню
+import { burger } from './functions/burger';
+
+//Acordeon
+import { accorderon } from './functions/accorderon';
+
+//header input
+import { inputComponent } from './components/inputHeaderComponent';
+
+//Footer cookie
+import { footerCookie } from './components/footerCookieComponent';
+
+//btn scrollTop
+import { scrollTop } from './components/scrollTopComponent';
+
+//Blocks
+import { bestItemMobButton } from './components/home/bestItem-mobButtonComponent';
