8c2e5de7632c93f8a576532628bc92244a9ac778
kulakovskyi <torchok9@gmail.com>
2023-01-12 16:32:22 +0200

    refactoring code on page3 and finaly work version

--- src/js/components/page3/progressBarComponent.js
+++ src/js/components/page3/progressBarComponent.js
@@ -3,35 +3,20 @@
 const progressCircles = document.querySelectorAll('.circle');
 const textCircles = document.querySelectorAll('.ratings__item-text');
 
-function circles(one, two, tree, fourth, five, six){
-    const progressFirstCircle = one * 20;
-    const progressSecondCircle = two * 20;
-    const progressThirdCircle = tree * 20;
-    const progressFourthCircle = fourth * 20;
-    const progressFiveCircle = five * 20;
-    const progressSixCircle = six * 20;
+const reviewNumberArray = [3.5, 2.0, 3.5, 3, 4.0, 4.3];
 
-    if(one > 4 || two > 4 || tree > 4 || fourth > 4 || five > 4){
-
-    }
-
+function circlesCalculate(numbers){
+    const progressCircleArray = numbers.map(item => { return item * 20; })
     for (let i= 0 ; i < progressCircles.length; i++) {
-        progressCircles[0].setAttribute("stroke-dasharray", progressFirstCircle + ' , 100')
-        progressCircles[1].setAttribute("stroke-dasharray", progressSecondCircle + ' , 100')
-        progressCircles[2].setAttribute("stroke-dasharray", progressThirdCircle + ' , 100')
-        progressCircles[3].setAttribute("stroke-dasharray", progressFourthCircle + ' , 100')
-        progressCircles[4].setAttribute("stroke-dasharray", progressFiveCircle + ' , 100')
-        progressCircles[5].setAttribute("stroke-dasharray", progressSixCircle + ' , 100')
-    }
-
-    for (let i= 0 ; i < textCircles.length; i++) {
-        textCircles[0].innerHTML = one.toFixed(1);
-        textCircles[1].innerHTML = two.toFixed(1);
-        textCircles[2].innerHTML = tree.toFixed(1);
-        textCircles[3].innerHTML = fourth.toFixed(1);
-        textCircles[4].innerHTML =  five.toFixed(1);
-        textCircles[5].innerHTML =  six.toFixed(1);
+        progressCircles[i].setAttribute("stroke-dasharray", progressCircleArray[i] + ' , 100');
+        textCircles[i].innerHTML = numbers[i].toFixed(1);
+        function colorCircles(number){
+            if(number >= 4) progressCircles[i].classList.add('green');
+            if(number < 4 && number >= 3) progressCircles[i].classList.add('yellow');
+            if(number < 3) progressCircles[i].classList.add('red');
+        }
+        colorCircles(numbers[i])
     }
 }
 
-circles(4.5, 2.0, 3.5, 5.0, 4.0, 4.3)
+circlesCalculate(reviewNumberArray)
